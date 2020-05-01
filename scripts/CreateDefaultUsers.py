fa56a2dbf98ff740119a43f61a0b45f8f83202f2
Burgess <KAB0328@daugherty.com>
2020-05-01 08:55:58 -0500

    Updated read command in CreateDefaultUsers.py

--- scripts/CreateDefaultUsers.py
+++ scripts/CreateDefaultUsers.py
@@ -4,30 +4,28 @@ import json
 
 def create_users():
     #Open JSON File which contains details for default users that need to be created and put it in a variable
-    f = open('resources/DefaultUsers.json')
-    data = json.load(f)
-    
-    #loop through each user's credentials and build a create user command from all present criteria
-    for i in data['DefaultUsers']:
-        createUser = 'airflow create_user'
-        #Add each argument to the create user command if it exists in the JSON object
-        if 'username' in i:
-            createUser += f" -u {i['username']}" #Must be unique
-        if 'role' in i:
-            createUser += f" -r {i['role']}" #Options include Admin, User, Op, Viewer, and Public
-        if 'email' in i:
-            createUser += f" -e {i['email']}" #Must be unique
-        if 'firstname' in i:
-            createUser += f" -f {i['firstname']}"
-        if 'lastname' in i:
-            createUser += f" -l {i['lastname']}"
-        if 'password' in i:
-            createUser += f" -p {i['password']}"
+    with open('resources/DefaultUsers.json', 'r') as f:
+        data = json.load(f)
+        
+        #loop through each user's credentials and build a create user command from all present criteria
+        for user in data['DefaultUsers']:
+            createUser = 'airflow create_user'
+            #Add each argument to the create user command if it exists in the JSON object
+            if 'username' in user:
+                createUser += f" -u {user['username']}" #Must be unique
+            if 'role' in user:
+                createUser += f" -r {user['role']}" #Options include Admin, User, Op, Viewer, and Public
+            if 'email' in user:
+                createUser += f" -e {user['email']}" #Must be unique
+            if 'firstname' in user:
+                createUser += f" -f {user['firstname']}"
+            if 'lastname' in user:
+                createUser += f" -l {user['lastname']}"
+            if 'password' in user:
+                createUser += f" -p {user['password']}"
 
-        #Execute the command to create an airflow user from the command line
-        os.system(f"{createUser}")
-
-    f.close()
+            #Execute the command to create an airflow user from the command line
+            os.system(f"{createUser}")
 
 if __name__ == "__main__":
     create_users()
